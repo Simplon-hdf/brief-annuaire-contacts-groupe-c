e568d093625a545abaa9fa1be63e85ee5408273b
Hazel <basile.cunuder@gmail.com>
2025-06-10 10:45:40 +0200

    feat(formulaire): import NgModule and NgModel

--- src/app/formulaire/formulaire.ts
+++ src/app/formulaire/formulaire.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'; 
-import { FormsModule } from '@angular/forms'; 
+import { Component, NgModule } from '@angular/core'; 
+import { FormsModule, NgModel } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
 
 @Component({
