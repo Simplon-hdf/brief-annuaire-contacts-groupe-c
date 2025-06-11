014c4ceda2a5adef5db194e7a4a1fde4e33eb031
tutuat <153525161+tutuat@users.noreply.github.com>
2025-06-11 09:19:55 +0200

    feat(footer.html): Create footer base

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { Footer } from "./footer/footer";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Footer],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
