ca84889a663c09dbf4e4760f158b1b6098be605e
tutuat <153525161+tutuat@users.noreply.github.com>
2025-06-11 15:17:19 +0200

    refactor(app): remove carte-contact component from app module and template

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
-import { CarteContact } from "./carte-contact/carte-contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, CarteContact],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
