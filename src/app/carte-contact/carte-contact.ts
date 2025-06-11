cab7ed56ddd653f32b2671218dc377d7758f48f8
tutuat <153525161+tutuat@users.noreply.github.com>
2025-06-11 13:48:14 +0200

    feat(carte-contact): Create component carte-contact

---
+++ src/app/carte-contact/carte-contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-contact',
+  imports: [],
+  templateUrl: './carte-contact.html',
+  styleUrl: './carte-contact.css'
+})
+export class CarteContact {
+
+}
