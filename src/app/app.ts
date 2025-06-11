d8b681dd74831d59da3e260758186ac2309692b6
tutuat <153525161+tutuat@users.noreply.github.com>
2025-06-11 14:56:40 +0200

    feat(carte-contact): implement contact card component with template and input properties

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
+import { CarteContact } from "./carte-contact/carte-contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, CarteContact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/carte-contact/carte-contact.ts
+++ src/app/carte-contact/carte-contact.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { CommonModule,} from '@angular/common';
+
+
+interface Contact {
+  prenom: string;
+  nom: string;
+  telephone: string;
+  email: string;
+  addresse: string;
+  type: 'client' | 'fournisseur';
+  imageUrl?: string;
+}
 
 @Component({
   selector: 'app-carte-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carte-contact.html',
   styleUrl: './carte-contact.css'
 })
 export class CarteContact {
-
+  @Input() contact: Contact | undefined;
 }
