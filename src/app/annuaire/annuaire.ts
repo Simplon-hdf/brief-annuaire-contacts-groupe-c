ab196f780b0c78a417a68f36101c46c870f33f10
Hazel <basile.cunuder@gmail.com>
2025-06-12 09:11:36 +0200

    feat(annuaire): add annuaire component

---
+++ src/app/annuaire/annuaire.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-annuaire',
+  imports: [],
+  templateUrl: './annuaire.html',
+  styleUrl: './annuaire.css'
+})
+export class Annuaire {
+}
